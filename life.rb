3a42c246ac64d92ba28dadca5d027dedd554c529
Jon Bauman <jongithub@shumi.fake>
2015-10-28 16:03:02 -0700

    Add print_board

--- life.rb
+++ life.rb
@@ -16,3 +16,17 @@ end
 def succ
   []
 end
+
+Cell = Struct.new(:row, :col)
+def print_board(on_cells)
+  sorted_on_cells = on_cells.map {|c| Cell.new(*c) }.sort_by(&:col).sort_by(&:row)
+  row_range = (sorted_on_cells.first.row)..(sorted_on_cells.last.row)
+  col_range = (sorted_on_cells.first.col)..(sorted_on_cells.last.col)
+  puts "Printing [#{row_range.begin}, #{col_range.begin}]..[#{row_range.end}, #{col_range.end}]"
+  row_range.each do |r|
+    col_range.each do |c|
+      print sorted_on_cells.include?(Cell.new(r, c)) ? "█" : " "
+    end
+    puts "…"
+  end
+end
