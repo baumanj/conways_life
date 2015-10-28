edd9fdf122e3b1459c854620e9b016a0d2cb9c28
Jon Bauman <jongithub@shumi.fake>
2015-10-28 16:32:24 -0700

    Add a failing test

--- life.rb
+++ life.rb
@@ -6,19 +6,40 @@ require "rspec"
 # the number of 'on' cells is three, the cell becomes 'on'. If the number of
 # cells is greater than three, the cell becomes 'off'.
 
+Cell = Struct.new(:row, :col)
+
 describe :succ do
-  it "turns a cell 'off' if the number of surrounding 'on' cells is < 2"
+  it "turns a cell 'off' if the number of surrounding 'on' cells is < 2" do
+    on_cell = Cell.new(1, 1) # use rand
+
+    (0...2).each do |num_surrounding|
+      on_cells = [on_cell] + surrounding_cells(on_cell).sample(num_surrounding)
+      expect(send(subject, on_cells)).to be_empty
+    end
+  end
+
   it "leaves a cell unchanged if the number of surrounding 'on' cells == 2"
   it "turns a cell 'on' if the number of surrounding 'on' cells == 3"
   it "turns a cell 'off' if the number of surrounding 'on' cells > 3"
 end
 
-def succ
-  []
+def succ(on_cells)
+  nil
 end
 
-Cell = Struct.new(:row, :col)
+def surrounding_cells(cell)
+  offsets = [-1, 0, +1]
+  offsets.product(offsets).reject {|x| x == [0,0] }.map do |ro, co|
+    Cell.new(cell.row + ro, cell.col + co)
+  end
+end
+
 def print_board(on_cells)
+  if on_cells.empty?
+    puts "No 'on' cells"
+    return
+  end
+
   sorted_on_cells = on_cells.map {|c| Cell.new(*c) }.sort_by(&:col).sort_by(&:row)
   row_range = (sorted_on_cells.first.row)..(sorted_on_cells.last.row)
   col_range = (sorted_on_cells.first.col)..(sorted_on_cells.last.col)
